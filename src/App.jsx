e3e0f5ca40b53a3dad1f497220e2e7d4b97540fc
core.pager=cat <core.pager=cat@gmail.com>
2024-12-21 22:29:19 +0100

    Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
-
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Highlights from "./Components/Highlights";
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <main className="bg-black">
+    <Navbar />
+   < Hero  />
+    <Highlights  />
+
+
+  </main>
   )
 }
 
